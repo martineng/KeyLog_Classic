adb98cf9bb49b165d070739c00ebfcbfa4396299
martineng <Martin.ECA.6651@gmail.com>
2018-11-08 17:33:03 +0800

    Database Connection Done

--- KeyLog_Classic/Program.cs
+++ KeyLog_Classic/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using BCrypt.Net;
 
 namespace KeyLog_Classic
 {
@@ -7,6 +10,52 @@ namespace KeyLog_Classic
         public static void Main(string[] args)
         {
 
+        }
+
+        // Print UI
+        private static void HomeMenu()
+        {
+            Console.WriteLine("*========================================*");
+            Console.WriteLine("*           KeyLog Classic               *");
+            Console.WriteLine("*             Ver. 1.0                   *");
+            Console.WriteLine("*========================================*");
+            Console.Write("\n");
+        }
+
+        // Request Credential for Database Connection
+        private static string DatabaseConnectionInfo()
+        {
+            string server = "", id = "", password = "";
+            const string database = "testPassword";
+
+            string connectionInfo = "";
+            MySqlConnection connection = new MySqlConnection();
+
+            Console.WriteLine("Please Enter ");
+            Console.WriteLine("Server:> ");
+            server = Console.ReadLine();
+
+            Console.WriteLine("Id:> ");
+            id = Console.ReadLine();
+
+            Console.WriteLine("Password:> ");
+            password = Console.ReadLine();
+
+            connectionInfo = "server=" + server + ";" +
+                             "User Id=" + id + ";" +
+                             "password=" + password + ";" +
+                             "database=" + database + ";";
+
+            return connectionInfo;
+        } // END DatabaseConnectionInfo();
+
+        // Open Connection with the database
+        private static MySqlConnection DatabaseConnection(string inConnectionInfo)
+        {
+            MySqlConnection connection = new MySqlConnection(inConnectionInfo);
+            connection.Open();
+
+            return connection;
         }
     }
 }
