62960f193ba35fa3ef7ae93927bacb2dc98635e6
martineng <Martin.ECA.6651@gmail.com>
2018-11-08 17:01:05 +0800

    Project Creation

---
+++ KeyLog_Classic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyLog_Classic
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
